c85192a7529f384242ce18271a06b678a0c4a5a2
Frank Geldner <frgeldne@cisco.com>
2021-01-29 12:24:20 +0100

    add new document

---
+++ DevNet-Python-questions.py
@@ -0,0 +1,17 @@
+# This file contains DevNet Associate questions
+
+list1 = ['Munich', 'Hamburg']
+list2 = [1, 2, 3, 4, 5, 6, 7, 8]
+list3 = list1 + list2
+
+print(list1)
+print(list2)
+print(list1[0])
+print(list2[3])
+
+print(list3)
+
+
+
+
+
